a79b90640b85cfd0e2399510064d850fac8c133a
2Xpro-pop <bay.asanov@mail.ru>
2024-01-20 08:07:55 +0600

    added mock data for additive

---
+++ Kassa.DataAccess/Additive.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassa.DataAccess;
+public class Additive : IModel
+{
+    public int Id
+    {
+        get; set;
+    }
+
+    public string Name
+    {
+        get; set;
+    } = null!;
+
+    public string CurrencySymbol
+    {
+        get; set;
+    } = null!;
+
+    public double Price
+    {
+        get; set;
+    }
+
+    public double Count
+    {
+        get; set;
+    }
+
+    public string Measure
+    {
+        get; set;
+    } = null!;
+
+    /// <summary>
+    /// Identifier of the product to which this additive belongs.
+    /// </summary>
+    public int ProductId
+    {
+        get; set;
+    }
+}
