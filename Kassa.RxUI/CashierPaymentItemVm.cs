54c3b0c1c0c550cc2ad12566e30ff9ddbd1156b5
2Xpro-pop <bay.asanov@mail.ru>
2024-02-16 20:26:38 +0600

    fixing bugs

---
+++ Kassa.RxUI/CashierPaymentItemVm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace Kassa.RxUI;
+public class CashierPaymentItemVm : ReactiveObject
+{
+    [Reactive]
+    public double Cost
+    {
+        get; set;
+    }
+
+    [Reactive]
+    public string CurrencySymbol
+    {
+        get; set;
+    }
+
+    [Reactive]  
+    public string Name
+    {
+        get; set;
+    }
+
+    [Reactive]
+    public ReactiveCommand<Unit, Unit> RemoveItem
+    {
+        get; set;
+    }
+}
