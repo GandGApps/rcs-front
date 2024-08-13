6f88071cd3b34abb8a3b7012909bea2d99ecbdd9
2Xpro-pop <bay.asanov@mail.ru>
2024-08-13 15:52:54 +0600

    added base dialog to select

---
+++ Kassa.RxUI/Dialogs/BaseSelectDialogViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI.Fody.Helpers;
+using ReactiveUI;
+
+namespace Kassa.RxUI.Dialogs;
+
+/// <summary>
+/// It's just non generic base class for select dialog.
+/// Don't use it directly. Use <see cref="BaseSelectDialogViewModel{TItem,TVm}"/>
+/// </summary>
+public abstract class BaseSelectDialogViewModel: DialogViewModel
+{
+    protected static readonly TimeSpan SearchThrottle = TimeSpan.FromMilliseconds(500);
+
+    [Reactive]
+    public string? SearchText
+    {
+        get; set;
+    }
+
+    [Reactive]
+    public bool IsKeyboardVisible
+    {
+        get; set;
+    }
+}
+
+public abstract class BaseSelectDialogViewModel<TItem, TVm> : BaseSelectDialogViewModel
+    where TItem : class
+    where TVm : class
+{
+    
+    [Reactive]
+    public ReadOnlyObservableCollection<TVm>? FilteredItems
+    {
+        get; protected set;
+    }
+
+    [Reactive]
+    public ReactiveCommand<TVm, Unit>? SelectCommand
+    {
+        get; protected set;
+    }
+
+    [Reactive]
+    public TVm? SelectedItem
+    {
+        get; set;
+    }
+}
---
+++ Kassa.Wpf/Dialogs/SelectionDialogBase.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Kassa.RxUI.Dialogs;
+
+namespace Kassa.Wpf.Dialogs;
+
+public abstract class SelectionDialogBase<T> : ClosableDialog<T> where T : BaseSelectDialogViewModel
+{
+    public static readonly DependencyProperty HeaderTemplateProperty = DependencyProperty.Register(
+        nameof(HeaderTemplate), typeof(DataTemplate), typeof(SelectionDialogBase<T>), new PropertyMetadata(default(DataTemplate)));
+
+    public static readonly DependencyProperty ItemTemplateProperty = DependencyProperty.Register(
+        nameof(ItemTemplate), typeof(DataTemplate), typeof(SelectionDialogBase<T>), new PropertyMetadata(default(DataTemplate)));
+
+    public static readonly DependencyProperty IsKeyboardVisibleProperty = DependencyProperty.Register(
+        nameof(IsKeyboardVisible), typeof(bool), typeof(SelectionDialogBase<T>), new PropertyMetadata(default(bool)));
+
+    public static readonly DependencyProperty IsKeyboardEnabledProperty = DependencyProperty.Register(
+        nameof(IsKeyboardEnabled), typeof(bool), typeof(SelectionDialogBase<T>), new PropertyMetadata(default(bool)));
+
+    static SelectionDialogBase()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(SelectionDialogBase<T>), new FrameworkPropertyMetadata(typeof(SelectionDialogBase<T>)));
+    }
+
+    private static void OnIsKeyboardVisibleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is SelectionDialogBase<T> dialog)
+        {
+            var value = (bool)e.NewValue;
+
+            if (!value)
+            {
+                dialog.IsKeyboardEnabled = false;
+            }
+        }
+    }
+
+
+    public DataTemplate HeaderTemplate
+    {
+        get => (DataTemplate)GetValue(HeaderTemplateProperty);
+        set => SetValue(HeaderTemplateProperty, value);
+    }
+
+    public DataTemplate ItemTemplate
+    {
+        get => (DataTemplate)GetValue(ItemTemplateProperty);
+        set => SetValue(ItemTemplateProperty, value);
+    }
+
+    public bool IsKeyboardVisible
+    {
+        get => (bool)GetValue(IsKeyboardVisibleProperty);
+        set => SetValue(IsKeyboardVisibleProperty, value);
+    }
+
+    public bool IsKeyboardEnabled
+    {
+        get => (bool)GetValue(IsKeyboardEnabledProperty);
+        set => SetValue(IsKeyboardEnabledProperty, value);
+    }
+
+    public SelectionDialogBase()
+    {
+        ClearValue(HeaderTemplateProperty);
+    }
+
+}
