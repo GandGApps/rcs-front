569a34c0fffa34297213584140c43262baf34d11
2Xpro-pop <bay.asanov@mail.ru>
2024-09-10 17:53:47 +0600

    adding base dialog

--- Kassa.Avalonia/AssemblyInfo.cs
+++ Kassa.Avalonia/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using Avalonia.Metadata;
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Kassa.Avalonia")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Kassa.Avalonia.Pages")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui", "Kassa.Avalonia.Controls")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Kassa.Avalonia.MarkupExtensions")]
