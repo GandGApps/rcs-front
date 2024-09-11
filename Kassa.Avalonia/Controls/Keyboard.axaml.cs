ba6a541bf67171e6cdaa48240c6ee22f6247b4aa
2Xpro-pop <bay.asanov@mail.ru>
2024-09-11 12:12:25 +0600

    added scrollbar

---
+++ Kassa.Avalonia/Controls/Keyboard.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Kassa.Avalonia;
+
+public partial class Keyboard : UserControl
+{
+    public Keyboard()
+    {
+        InitializeComponent();
+    }
+}
