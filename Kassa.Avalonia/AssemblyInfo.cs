d262ae0658e6f85955961fd2ad5ab6be1a22cbb0
2Xpro-pop <bay.asanov@mail.ru>
2024-10-07 12:40:40 +0600

    remove unnecessary projects

--- Kassa.Avalonia/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿
-using Avalonia.Metadata;
-
-[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Kassa.Avalonia")]
-[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Kassa.Avalonia.Pages")]
-[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Kassa.Avalonia.Controls")]
-[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Kassa.Avalonia.MarkupExtensions")]
-[assembly: XmlnsDefinition("https://github.com/avaloniaui", "Kassa.Avalonia.Dialogs")]
